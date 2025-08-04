62c460a917432b2d9f985266fb4977203c2fcdbe
matslyk0 <matslyk0@gmail.com>
2025-08-04 11:15:59 +0100

    feature: finish database setup; flesh out docker-compose

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = "database"
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ backend/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, index=True)
+    hashed_password = Column(String, index=True)
+
+class Repositories(Base):
+    __tablename__ = 'repositories'
+
+    id = Column(Integer, primary_key=True, index=True)
+    owner = Column(String, index=True)
+    name = Column(String, index=True)
+
+class Analysis(Base):
+    __tablename__ = 'analysis'
+
+    id = Column(Integer, primary_key=True, index=True)
+    repository_id = Column(Integer, ForeignKey("repositories.id"))
+    metric1 = Column(String, index=True)
+    metric2 = Column(String, index=True)
+    created_at = Column(String, index=True)
+
+class Dummy(Base):
+    __tablename__ = 'dummy'
+
+    id = Column(Integer, primary_key=True, index=True)
+    test = Column(String, index=True)
