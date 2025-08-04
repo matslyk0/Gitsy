855522a05d8417085fe652e789aa208b8cf96061
matslyk0 <matslyk0@gmail.com>
2025-08-04 11:15:07 +0100

    fix: refactor github api call methods

--- backend/analysis_engine.py
+++ backend/analysis_engine.py
@@ -1,10 +1,17 @@
-import requests
+import requests, json
 
-# repository URLs will be pasted in the format https://github.com/{owner}/{repo}
+def get_repo_commits(repo_url: str):
+    """
+    Obtains the commits of a public repository
 
-def get_commits(repo_url):
+    Args:
+        repo_url (str): string in the format https://github.com/{user}/{repo}
 
-    # isolating the {owner}/{repo} and stitching it with the API endpoint
+    Returns:
+        list, dict: a .json of the commits
+        str: an error message if obtaining info failed
+    """
+
     info = repo_url.removeprefix("https://github.com/")
     url = f"https://api.github.com/repos/{info}/commits"
 
@@ -14,5 +21,28 @@ def get_commits(repo_url):
     else:
         return f"Error in retrieving data. Status code {response.status_code}"
 
-repo_info = get_commits("https://github.com/matslyk0/Gitsy")
-print(repo_info)
+def get_user_repos(user_url: str):
+    """
+    Obtains the public repositories of a user
+
+    Args:
+        user_url: string in the format https://github.com/{owner}
+
+    Returns:
+        list, dict: a .json of the commits
+        str: an error message if obtaining info failed
+    """
+
+    info = user_url.removeprefix("https://github.com/")
+    url = f"https://api.github.com/users/{info}/repos"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error in retrieving data. Status code {response.status_code}"
+
+output = get_repo_commits("https://github.com/matslyk0/Gitsy")
+if not isinstance(output, str):
+    print(json.dumps(output, indent=4))
+#print(get_user_repos("matslyk0"))
