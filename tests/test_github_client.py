c542f3acf72659d8570e672b9b04f3650319b5ea
Matty <matslyk0@gmail.com>
2025-09-22 18:07:26 +0200

    fix: format test_github_client in accordance to PEP8

--- tests/test_github_client.py
+++ tests/test_github_client.py
@@ -1,22 +1,27 @@
 import asyncio
 import backend.github_client as github_client
 
+
 def test_get_commits():
-    commits = asyncio.run(github_client.get_commits("https://github.com/matslyk0/Gitsy"))
+    repo_url = "https://github.com/matslyk0/Gitsy"
+    commits = asyncio.run(github_client.get_commits(repo_url))
     assert commits
 
 
 def test_get_commit_info():
     sha = "42e3978c91d90c51c1a7a0e154bd92fe5021783f"
-    commit = asyncio.run(github_client.get_commit_info("https://github.com/matslyk0/Gitsy", sha))
+    repo_url = "https://github.com/matslyk0/Gitsy"
+    commit = asyncio.run(github_client.get_commit_info(repo_url, sha))
     assert commit
 
 
 def test_get_issues():
-    issues = asyncio.run(github_client.get_issues("https://github.com/matslyk0/Gitsy"))
+    repo_url = "https://github.com/matslyk0/Gitsy"
+    issues = asyncio.run(github_client.get_issues(repo_url))
     assert issues
 
 
 def test_get_pulls():
-    pulls = asyncio.run(github_client.get_pulls("https://github.com/matslyk0/Gitsy", state="closed"))
+    repo_url = "https://github.com/matslyk0/Gitsy"
+    pulls = asyncio.run(github_client.get_pulls(repo_url, state="closed"))
     assert pulls
