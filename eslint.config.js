6df042895ad326aa366fc433ddf6fe1dd26e4f2f
Matty <matslyk0@gmail.com>
2025-09-30 15:27:09 +0200

    feature: set up react with vite

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,28 @@
-// eslint.config.js at the project root
 import js from "@eslint/js";
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
-import prettier from "eslint-config-prettier";
 import globals from "globals";
+import react from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
-    js.configs.recommended,
-    {
-        plugins: {
-            react,
-            "react-hooks": reactHooks
-        },
-        languageOptions: {
-            globals: globals.browser,
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
-                ecmaFeatures: {
-                    jsx: true
-                }
-            }
-        },
-        rules: {
-            ...react.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules
-        },
-        settings: {
-            react: {
-                version: "detect"
-            }
-        }
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      react.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    prettier,
-];
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+    },
+  },
+  prettier,
+]);
