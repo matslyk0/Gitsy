696ec89655e429847dafe140e2ad924eb6d18f93
matslyk0 <matslyk0@gmail.com>
2025-10-13 20:41:00 +0100

    feature: resize <main> to fill out the full Home page

--- frontend/gitsy/src/pages/Home/Home.jsx
+++ frontend/gitsy/src/pages/Home/Home.jsx
@@ -6,10 +6,19 @@ import Footer from "../../components/Footer/Footer.jsx";
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const mainStyles = {
+    justifyContent: "center",
+    display: "flex",
+    alignItems: "center",
+    flex: "1",
+    flexDirection: "column",
+    border: "0px solid white" /* for debugging */,
+  };
+
   return (
     <>
       <Banner />
-      <main>
+      <main style={mainStyles}>
         <div className={styles.welcome}>
           <h1>Welcome to Gitsy!</h1>
         </div>
