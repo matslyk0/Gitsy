ad36ed0b72f51e6e3b4a14613709542d86e8c3a6
matslyk0 <matslyk0@gmail.com>
2025-10-31 12:28:36 +0000

    feature: remove unused validation schemas in schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,30 +1,5 @@
 from pydantic import BaseModel
 from typing import Dict
-from datetime import datetime
-
-
-class Repository(BaseModel):
-    repo_id: int
-    repo_owner: str
-    repo_name: str
-
-    class Config:
-        # needed by Pydantic for SQLAlchemy model to JSON conversion
-        from_attributes = True
-
-
-class Report(BaseModel):
-    report_id: int
-    repo_id: int
-
-    commit_frequency: float
-    code_churn: Dict[str, int]
-    issues_close_time: float
-    pulls_close_time: float
-
-    created_at: datetime
-    last_updated: datetime
-
 
 class ReportOut(BaseModel):
     commit_frequency: float
