f32db17d39d1ec19d6677b924757621b4fc616b3
matslyk0 <matslyk0@gmail.com>
2025-11-07 13:00:35 +0000

    feature: restructure create report and improve error parsing

--- frontend/gitsy/src/pages/CreateReport/CreateReport.jsx
+++ frontend/gitsy/src/pages/CreateReport/CreateReport.jsx
@@ -1,6 +1,9 @@
+import styles from "./CreateReport.module.css";
+
 import Banner from "../../components/Banner/Banner.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import styles from "./CreateReport.module.css";
+import Card from "../../components/Card/Card.jsx";
+
 import axios from "axios";
 import React, { useState } from "react";
 
@@ -15,43 +18,57 @@ function CreateReportForm({ onClick }) {
 
 function ReportDisplay({ report }) {
   const errorMessages = {
-    "InsufficientDataError()": "Not enough data!",
-    "GitHubAPIError()": "GitHub request failed!",
-    "GitHubTimeOutError()": "GitHub took too long!",
-    "RepoTooLargeError()": "Repository must be under 10,000 commits!",
+    InsufficientDataError: "Not enough data!",
+    GitHubAPIError: "GitHub request failed!",
+    GitHubTimeOutError: "GitHub took too long!",
+    RepoTooLargeError: "Repository must be under 10,000 commits!",
   };
 
-  const commitFrequency =
-    errorMessages[report?.commit_frequency] ?? report?.commit_frequency;
-  const issuesCloseTime =
-    errorMessages[report?.issues_close_time] ?? report?.issues_close_time;
-  const pullsCloseTime =
-    errorMessages[report?.pulls_close_time] ?? report?.pulls_close_time;
+  const commitFrequency = (() => {
+    if (!report) return null;
 
-  let codeChurn = "";
-  if (errorMessages[report?.code_churn] == null) {
-    if (report?.code_churn != null) {
-      codeChurn = (
-        <ul>
-          <li>Additions: {report?.code_churn?.additions}</li>
-          <li>Deletions: {report?.code_churn?.deletions}</li>
-          <li>Total: {report?.code_churn?.total}</li>
-          <li>Net: {report?.code_churn?.net}</li>
-        </ul>
-      );
-    }
-  } else {
-    codeChurn = errorMessages[report?.code_churn];
-  }
+    const { data, error } = report.commit_frequency;
+    if (error) return errorMessages[error] || "Unknown error occurred!";
+
+    return data;
+  })();
+  const issuesCloseTime = (() => {
+    if (!report) return null;
+
+    const { data, error } = report.issues_close_time;
+    if (error) return errorMessages[error] || "Unknown error occurred!";
+
+    return data;
+  })();
+  const pullsCloseTime = (() => {
+    if (!report) return null;
+
+    const { data, error } = report.pulls_close_time;
+    if (error) return errorMessages[error] || "Unknown error occurred!";
+
+    return data;
+  })();
+  const codeChurn = (() => {
+    if (!report) return null;
+
+    const { data, error } = report.code_churn;
+    if (error) return errorMessages[error] || "Unknown error occurred!";
+
+    const { additions, deletions, total, net } = data;
+    return `
+      Additions: ${additions},
+      Deletions: ${deletions},
+      Total: ${total},
+      Net: ${net}
+      `;
+  })();
 
   return (
     <div className={styles.reportDisplay}>
-      <ul>
-        <li>Time Between Commits (hrs): {commitFrequency}</li>
-        <li>Issues Close Time (hrs): {issuesCloseTime}</li>
-        <li>Pulls Close Time (hrs): {pullsCloseTime}</li>
-      </ul>
-      Code Churn (Lines of Code): {codeChurn}
+      <Card metricName="Commit Frequency" metricData={commitFrequency} />
+      <Card metricName="Issues Close Time" metricData={issuesCloseTime} />
+      <Card metricName="Pulls Close Time" metricData={pullsCloseTime} />
+      <Card metricName="Code Churn" metricData={codeChurn} />
     </div>
   );
 }
@@ -66,7 +83,7 @@ export default function CreateReport() {
     justifyContent: "center",
   };
 
-  const [report, setReport] = useState([]);
+  const [report, setReport] = useState(null);
 
   async function CallAnalysisFunction() {
     const enteredUrl = document.getElementById("urlInput").value;
